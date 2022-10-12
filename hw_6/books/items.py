5886ff0875b180229ed508b9db4ebe196babbe77
maximmigood <maksim.mikheev@gmail.com>
2022-10-12 22:49:49 +0300

    Домашняя работа к уроку 6

---
+++ hw_6/books/items.py
@@ -0,0 +1,18 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BooksItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    _id = scrapy.Field()
+    link = scrapy.Field()
+    name = scrapy.Field()
+    author = scrapy.Field()
+    price = scrapy.Field()
+    price_full = scrapy.Field()
+    rating = scrapy.Field()
